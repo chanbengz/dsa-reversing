5d5a118ad1f6e511972e918d30457c10ed5f64a9
chanbengz <chanben04gz@gmail.com>
2025-03-27 23:46:30 +0800

    atc: functioning for now :)

--- src/atc/gao.c
+++ src/atc/gao.c
@@ -4,44 +4,48 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define TEST_SIZE 1
 #define BLEN (4096 << 16)
 #define DSA_OP_FLAG_US (1 << 16)
 
-char *probe_arr;
+struct dsa_completion_record* probe_arr;
 struct wq_info wq_info;
 struct dsa_hw_desc desc = {};
-struct dsa_completion_record comp __attribute__((aligned(32))) = {};
+/*struct dsa_completion_record comp __attribute__((aligned(32))) = {};*/
+
 
 uint64_t probe(void* addr)
 {
-    uint64_t start, end;
     int retry = 0;
+    uint64_t start = 0, end = 0;
+    struct dsa_completion_record* comp = (struct dsa_completion_record*) addr;
 
     // Initialize the descriptor
-    comp.status            = 0;
-    desc.src_addr          = (uintptr_t) addr;
-    memset(addr, 0, 8);
+    comp->status            = 0;
+    desc.completion_addr    = (uintptr_t) comp;
+    // desc.src_addr          = (uintptr_t) addr;
+    // memset(addr, 0, 8);
 
 retry:
     enqcmd(wq_info.wq_portal, &desc);
 
     start = rdtsc();
-    while (comp.status == 0 && retry++ < MAX_COMP_RETRY) {
+    while (comp->status == 0 && retry++ < MAX_COMP_RETRY) {
         umonitor(&(comp));
-        if (comp.status == 0) {
+        if (comp->status == 0) {
         uint64_t delay = __rdtsc() + UMWAIT_DELAY;
             umwait(UMWAIT_STATE_C0_1, delay);
         }
     }
     end = rdtsc();
 
-    if (comp.status != DSA_COMP_SUCCESS) {
-        printf("Failed: %d Bytes Completed: %d\n", comp.status, comp.bytes_completed);
-        printf("Address: %p\n", (void *)comp.fault_addr);
-        if (op_status(comp.status) == DSA_COMP_PAGE_FAULT_NOBOF) {
-            int wr = comp.status & DSA_COMP_STATUS_WRITE;
+    if (comp->status != DSA_COMP_SUCCESS) {
+        printf("Failed: %d Bytes Completed: %d\n", comp->status, comp->bytes_completed);
+        printf("Address: %p\n", (void *)comp->fault_addr);
+        if (op_status(comp->status) == DSA_COMP_PAGE_FAULT_NOBOF) {
+            int wr = comp->status & DSA_COMP_STATUS_WRITE;
             volatile char *t;
-            t = (char *)comp.fault_addr;
+            t = (char *)comp->fault_addr;
             wr ? *t = *t : *t;
             goto retry;
         }
@@ -52,7 +56,7 @@ retry:
 
 void profile(void* base, int w, int s)
 {
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < TEST_SIZE; i++) {
         for (int j = 1; j <= w + 1; j++) {
             probe(base + j * s * 4096);
         }
@@ -64,31 +68,24 @@ int main(int argc, char *argv[])
     uint64_t start, end;
     int retry = 0;
 
-    probe_arr = malloc(BLEN);
+    probe_arr = (struct dsa_completion_record *)aligned_alloc(32, BLEN);
     if (map_wq(&wq_info)) return EXIT_FAILURE;
     // warm up
-    comp.status = 0;
+    probe_arr[0].status = 0;
     desc.opcode = DSA_OPCODE_NOOP;
     desc.flags = IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR;
-    desc.completion_addr = (uintptr_t) &comp;
+    desc.completion_addr = (uintptr_t) &probe_arr[0];
     enqcmd(wq_info.wq_portal, &desc);
     start = rdtsc();
-    while (comp.status == 0) _mm_pause();
+    while (probe_arr[0].status == 0) _mm_pause();
     end = rdtsc();
     printf("Warmup: %ld\n", end - start);
 
-    desc.opcode            = DSA_OPCODE_COMPVAL;
-    desc.flags             = IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV;
-    desc.comp_pattern      = 0x0;
-    desc.xfer_size         = 8;
-    desc.expected_res      = 0;
-    desc.completion_addr   = (uintptr_t) &comp;
-
-    // benchmark ATC
+    // Benchmarking ATC
     if (argc == 3) {
-        printf("Testing...\n");
         int w = atoi(argv[1]);
         int s = atoi(argv[2]);
+        printf("Testing W = %d, S = %d\n", w, s);
         profile(probe_arr, w, s);
     }
 
