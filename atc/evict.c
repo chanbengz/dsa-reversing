b17431423a7f54ef30972c06b1ad25316cc477ca
chanbengz <chanben04gz@gmail.com>
2025-04-10 22:42:44 +0800

    atc: cross-engine eviction not applicable

--- atc/evict.c
+++ atc/evict.c
@@ -12,55 +12,31 @@ struct dsa_hw_desc desc = {};
 #define MAX_OFFSET 22
 uint64_t results[MAX_OFFSET + 1][TESTS_PER_PROBE];
 
+int map_another_wq(struct wq_info* wq_info) {
+    int fd = open("/dev/dsa/wq0.1", O_RDWR);
+    void* wq_portal = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (wq_portal == MAP_FAILED) {
+        if (errno == EPERM && is_write_syscall_success(fd)) {
+            wq_info->wq_mapped = false;
+            wq_info->wq_fd = fd;
+            return 0;
+        }
+        return -errno;
+    }
+    wq_info->wq_portal = wq_portal;
+    wq_info->wq_mapped = true;
+    wq_info->wq_fd = -1;
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     probe_arr = (struct dsa_completion_record *)aligned_alloc(32, BLEN);
     memset(probe_arr, 0, BLEN >> 10);
-    if (map_wq(&wq_info)) return EXIT_FAILURE;
-    // Warm up
-    probe_arr[0].status = 0;
-    desc.opcode = DSA_OPCODE_NOOP;
-    desc.flags = IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR;
-    desc.completion_addr = (uintptr_t) &probe_arr[0];
-    enqcmd(wq_info.wq_portal, &desc);
-    while (probe_arr[0].status == 0) _mm_pause();
-
-    // Benchmarking ATC
-    void* base = probe_arr;
-    probe(base + 4096);
-    printf("Cache miss: %ld\n", probe(base));
-    printf("Cache hit:  %ld\n", probe(base));
-
-    // Different tests
-    switch (argc) {
-        case 2:
-            probe(base); // let base be in cache
-            printf("Sleeping %s seconds\n", argv[1]);
-            printf("start: %ld\n", probe(base));
-            sleep(atoi(argv[1]));
-            __builtin_ia32_lfence();
-            printf("end:   %ld\n", probe(base));
-            break;
-
-        default:
-            for (int j = 0; j < TESTS_PER_PROBE; j++) {
-                for (int i = 12; i <= MAX_OFFSET; i++) {
-                    probe(base);
-                    results[i][j] = probe(base + (4096ull << i));
-                    probe(base);
-                }
-            }
-
-            // ----- header
-            for (int i = 12; i <= MAX_OFFSET; i++) printf("| %4d ", i); printf("|\n");
-            for (int i = 12; i <= MAX_OFFSET; i++) printf("|------");   printf("|\n");
-            // ----- body
-            for (int j = 0; j < TESTS_PER_PROBE; j++) {
-                for (int i = 12; i <= MAX_OFFSET; i++) printf("| %4ld ", results[i][j]);
-                printf("|\n");
-            }
-            break;
-    }
+    
+    // if (map_wq(&wq_info)) return EXIT_FAILURE;
+    if (map_another_wq(&wq_info)) return EXIT_FAILURE;
+    probe(probe_arr);
 
     return 0;
 }
@@ -70,6 +46,9 @@ uint64_t probe(void* addr)
 {
     uint64_t start, end, retry = 0, ret = 0;
     struct dsa_completion_record* comp = (struct dsa_completion_record*) addr;
+    probe_arr[0].status = 0;
+    desc.opcode = DSA_OPCODE_NOOP;
+    desc.flags = IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR;
     desc.completion_addr = (uintptr_t) comp;
     memset(comp, 0, 8);
 
--- playground/README.md
+++ playground/README.md
@@ -1,2 +1,22 @@
 # Playground of DSA
 
+## Experiements
+
+We tested if contention can be established in shared work queues (SWQ),
+with similar approaches as in the reverse-engineer of ROB. The methodology is
+pretty straightforward:
+1. Submit
+
+As we were not sured about the feasibility, we tried at first with fulling
+the work queue with outstanding work descriptors. Then we started another process
+to submit work descriptors to the work queue to see if it would encounter a failure.
+
+Next, we 
+
+## Deprecated
+
+Those experiments below proved that some of our hypothesis are wrong,
+or they do not concern the paper, so they are deprecated.
+
+- [Constant Time Test](./cmp-flush.c) Test `Compare` and `Flush` to see if they are implemented in constant time. Experiment showed that `Compare` is constant time, but `Flush` is not. Using `Flush` to implement Flush+Reload and Flush+Flush is the same as using `clflush` but could be less accurate and slower.
+- [Out of Bound Read/Write](./bound.c) Try `Memmory Move` with out-of-bound accesses. Results showed that it is possible to read/write memory outside of the buffer, similar to libc's `memcpy`.
