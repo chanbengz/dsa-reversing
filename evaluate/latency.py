70a9b4c8c4e17b50a08b914e6a0d1239e3727b73
chanbengz <chanben04gz@gmail.com>
2025-07-28 14:38:58 +0800

    fix: figures

--- evaluate/latency.py
+++ evaluate/latency.py
@@ -4,13 +4,14 @@ import numpy as np
 hit = np.fromfile("data/devtlb-hit.txt", dtype=int, sep="\n")
 miss = np.fromfile("data/devtlb-miss.txt", dtype=int, sep="\n")
 
+plt.figure(figsize=(4, 3))
 plt.rcParams['font.family'] = 'Optima'
-plt.hist(hit, bins=80, alpha=0.5, label='base', range=(200, 1200))
-plt.hist(miss, bins=80, alpha=0.5, label='base+offset', range=(200, 1200))
+plt.hist(hit, bins=200, alpha=0.5, label='base', range=(200, 1200))
+plt.hist(miss, bins=200, alpha=0.5, label='base+offset', range=(200, 1200))
 
-plt.xlabel('Latency (Cycles)', fontsize=16)
-plt.ylabel('Frequency', fontsize=16)
+plt.xlabel('Latency (Cycles)', fontsize=13)
+plt.ylabel('Frequency', fontsize=13)
 plt.tick_params(axis='both', which='major', labelsize=13)
-plt.legend()
+plt.legend(fontsize=11)
 
 plt.savefig('data/latency.pdf', format='pdf', bbox_inches='tight')
